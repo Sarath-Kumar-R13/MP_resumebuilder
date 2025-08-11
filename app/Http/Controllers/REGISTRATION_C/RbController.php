e9db7e4341d53f853fb03359e151d454df79ccc9
Sarath Kumar R <sarathkumarr154@gmail.com>
2025-08-11 15:46:36 +0530

    initial commit

--- app/Http/Controllers/REGISTRATION_C/RbController.php
+++ app/Http/Controllers/REGISTRATION_C/RbController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\resumeModel;
+
 
 class RbController extends Controller
 {
@@ -13,38 +15,53 @@ class RbController extends Controller
     
     public function regform(){
         return view('Registration/register');
-    }
-    public function register(Request $request){
-        $validateData=$request->validate([
-            'name'=>'required|string',
-            'email'=>'required|string|email|regex:/^[^\s@]+@[^\s@]+\.[^\s@]+$/',
-            'phone'=>'required|numeric|min:10',
-            'password'=>'required|string',
-            'confirm_password'=>'required|string|same:password'
-        ]);
-        $name=$request->post('name');
-        $email=$request->post('email');
-        $phone=$request->post('phone');
-        $password=$request->post('password');
-        $confirm_password=$request->post('confirm_password');
+     }
+    // public function register(Request $request){
+    //     $validateData=$request->validate([
+    //         'name'=>'required|string',
+    //         'email'=>'required|string|email|regex:/^[^\s@]+@[^\s@]+\.[^\s@]+$/',
+    //         'phone'=>'required|numeric|min:10',
+    //         'password'=>'required|string',
+    //         'confirm_password'=>'required|string|same:password'
+    //     ]);
+    //     $name=$request->post('name');
+    //     $email=$request->post('email');
+    //     $phone=$request->post('phone');
+    //     $password=$request->post('password');
+    //     $confirm_password=$request->post('confirm_password');
         
-        $user=new resumeModel();
-        $user->name=$name;
-        $user->email=$email;
-        $user->phone=$phone;
-        $user->password=bcrypt($password);
+    //     $user=new resumeModel();
+    //     $user->name=$name;
+    //     $user->email=$email;
+    //     $user->phone=$phone;
+    //     $user->password=bcrypt($password);
 
-        $user->save();
+    //     $user->save();
+   
+
+        public function register(Request $request){
+            $validated = $request->validate([
+                'name' => 'required|string|min:5|max:255',
+                'email' => 'required|email|unique:users,email',
+                'phone' => 'required|digits:10',
+                'password' => 'required|string|min:8|confirmed'
+            ]);
+
+
+            resumeModel::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'phone' => $validated['phone'],
+                'password' => Hash::make($validated['password']),
+            ]);
+
+    // return back()->with('success', 'Registration successful!');
         return redirect('login');
-    }
+    }   
     
-    // public function logout(Request $request){
-    //     Auth:logout();
-    //     $request->session()->invalidate();
-    //     $request->session()->regenerateToken();
-    //     return redirect('index');
-    // }
-    public function resumeV(){
+    
+       public function resumeV(){
         return view('ResumeTemplate.resume');
     }
 }
+
--- app/Http/Controllers/RESUME_C/OrganizationsController.php
+++ app/Http/Controllers/RESUME_C/OrganizationsController.php
@@ -7,5 +7,22 @@ use Illuminate\Http\Request;
 
 class OrganizationsController extends Controller
 {
-    //
+    public function mainResume6(){
+        return view('ResumeTemplate.resume');
+    }
+
+    public function organ(Request $request){
+        $validated=$request->validate([
+            'organizations'=>'required|string|max:255',
+            'from_date'=>'required|date',
+            'to_date'=>'required|date'
+        ]);
+
+        OrganizationsModel::create([
+            'organizations'=>$validated['organizations'],
+            'from_date'=>$validated['from_date'],
+            'to_date'=>$validated['to_date']
+        ]);
+        return back()->with('success','Recent workplace added successfully');
+    }
 }
